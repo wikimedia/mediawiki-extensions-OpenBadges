82e135d31e2fd11d444d5cc906cfff77bac30b7a
zoranzoki21 <zorandori4444@gmail.com>
2019-11-19 02:59:49 +0100

    Move some files in directories for them
    Just to separate everything in order to know what is what.
    
    Change-Id: I535b3b0cae3ae229721e168a8dc796583810a294

--- OpenBadgesHooks.php
+++ OpenBadgesHooks.php
@@ -16,9 +16,9 @@ class OpenBadgesHooks {
 	 */
 	public static function onLoadExtensionSchemaUpdates( $updater = null ) {
 		$updater->addExtensionTable( 'openbadges_class',
-			__DIR__ . '/OpenBadgesClass.sql' );
+			__DIR__ . '/sql/OpenBadgesClass.sql' );
 		$updater->addExtensionTable( 'openbadges_assertion',
-			__DIR__ . '/OpenBadgesAssertion.sql' );
+			__DIR__ . '/sql/OpenBadgesAssertion.sql' );
 		return true;
 	}
 }
